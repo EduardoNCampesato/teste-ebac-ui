89a05d7e4242386e79442e3b00e2d00dd831a5ab
Eduardo <eduardo.campesato.7@gmail.com>
2022-03-17 12:47:30 -0300

    Teste ui completo

---
+++ cypress/integration/pre-cadastro.spec.js
@@ -0,0 +1,31 @@
+/// <reference types="cypress" />
+var faker = require('faker-br');
+const Faker = require('faker-br/lib');
+const Fake = require('faker-br/lib/fake');
+
+context('Funcionalidade pré cadastro', () => {
+
+    beforeEach(() => {
+        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+    }); 
+
+    it.only('Deve efetuar o pré cadastro com sucesso', () => {
+        
+        cy.get('#reg_email').type(faker.internet.email())
+        cy.get('#reg_password').type(faker.internet.password())
+        cy.get(':nth-child(4) > .button').click()
+
+        cy.get('.woocommerce-MyAccount-navigation-link--edit-account > a').click()
+        cy.get('#account_first_name').type(faker.name.firstName())
+        cy.get('#account_last_name').type(faker.name.lastName())
+        cy.get('.woocommerce-Button').click()
+
+        cy.get('.woocommerce-message').should('contain' , 'Detalhes da conta modificados com sucesso.')
+    });
+    
+
+
+
+        
+
+})
