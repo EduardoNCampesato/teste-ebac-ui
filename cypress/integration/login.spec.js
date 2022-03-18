93f6e653ab18e9a0c39e7ce1948420f210733e9d
Eduardo <eduardo.campesato.7@gmail.com>
2022-03-18 17:23:12 -0300

    Teste ui completo 2.0 - Usando fixtures e arquivo de dados

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -1,9 +1,10 @@
 /// <reference types="cypress" />
+const perfil = require('../fixtures/perfil.json')
 
 context('Funcionalidade Login', () => {
 
     beforeEach(() => {
-        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')    
+        cy.visit('minha-conta')    
     });
     
     
@@ -15,6 +16,25 @@ context('Funcionalidade Login', () => {
 
         cy.get('.page-title').should('contain' , 'Minha conta')
     })
+
+    it('Deve fazer o login com sucesso - Usando arquivo de dados', () => {
+        cy.get('#username').type(perfil.usuario)
+        cy.get('#password').type(perfil.senha)
+        cy.get('.woocommerce-form > .button').click()
+
+        cy.get('.page-title').should('contain' , 'Minha conta')
+        
+    });
+
+    it.only('Deve fazer login com sucesso - Usando fixtures', () => {
+        cy.fixture('perfil.json').then(dados => {
+            cy.get('#username').type(dados.usuario)
+        cy.get('#password').type(dados.senha, {log: false})
+        cy.get('.woocommerce-form > .button').click()
+
+        cy.get('.page-title').should('contain' , 'Minha conta')
+        })
+    });
 
     it('Deve exibir uma mensagem de erro ao inserir usuário inválido', () => { 
         cy.get('#username').type('ebac@teste.com')
